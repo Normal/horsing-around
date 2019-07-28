d7849a834bbd1e69ddc783dc8abf0b034703b02f
George Yarish <george.yarish@macys.com>
2019-07-28 18:07:07 +0200

    added stdin scanner

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Running instructions
 
     sbt assembly
-    java -jar target/scala-2.12/horsing-around.jar
+    cat data/sample-input.json | java -jar target/scala-2.12/horsing-around.jar
     
 
 ### Tests
--- src/main/scala/bojack/App.scala
+++ src/main/scala/bojack/App.scala
@@ -1,12 +1,17 @@
 package bojack
 
+import java.util.Scanner
+
 import com.typesafe.scalalogging.StrictLogging
+import scala.collection.JavaConverters._
 
 object App extends StrictLogging {
 
   def main(args: Array[String]): Unit = {
-
     logger.info("It is alive")
+
+    val input = new Scanner(System.in).asScala.mkString
+    logger.info(input)
   }
 
 }
