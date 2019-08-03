1eb14a9acaa69f7619805752f2c05e9409719045
George Yarish <george.yarish@macys.com>
2019-08-03 13:41:02 +0200

    reimplement jobqueue with queues

--- src/main/scala/bojack/JobQueue.scala
+++ src/main/scala/bojack/JobQueue.scala
@@ -2,23 +2,59 @@ package bojack
 
 import bojack.model.{Agent, Job}
 
+import scala.collection.mutable
+
+/**
+  * The main implementation idea is to create two queues for each job type.
+  * One queue for urgent jobs and other for normal jobs.
+  *
+  * So it creates 2 * num_of_types queues which separated into two maps by urgent flag.
+  * All elements sorted by initial order in input jobs seq.
+  *
+  */
 class JobQueue(jobs: Seq[Job]) {
 
-  val (highPriority, normalPriority) = jobs.partition(_.urgent)
+  // save initial order for correct priority evaluation
+  private val jobsWithIndex: Seq[(Job, Int)] = jobs.zipWithIndex
 
-  def dequeue(agent: Agent): (Option[Job], JobQueue) = {
+  private val (urgentJobs, normalJobs) = jobsWithIndex.partition(_._1.urgent)
 
-    def find(jobs: Seq[Job], skills: Seq[String]): Option[Job] =
-      jobs.find(j => skills.contains(j.`type`))
+  private val highPriority: Map[String, mutable.Queue[(Job, Int)]] = createQueues(urgentJobs)
+  private val normalPriority: Map[String, mutable.Queue[(Job, Int)]] = createQueues(normalJobs)
 
-    val candidate: Option[Job] =
-      find(highPriority, agent.primarySkills)
-      .orElse(find(highPriority, agent.secondarySkills))
-      .orElse(find(normalPriority, agent.primarySkills))
-      .orElse(find(normalPriority, agent.secondarySkills))
+  private def createQueues(jobs: Seq[(Job, Int)]): Map[String, mutable.Queue[(Job, Int)]] =
+    jobs
+      .groupBy { case(job, index) => job.`type` }
+      .view
+      // my apologies for not functional approach here
+      .mapValues(seq => mutable.Queue.from(seq.sortBy { case(job, index) => index}))
+      .toMap
 
-    val queue = candidate.map(j => new JobQueue(jobs.filterNot(_.id == j.id))).getOrElse(this)
-    (candidate, queue)
+  /**
+    * Retrieving of the most relevant job by passed skill set and map of queues.
+    * Supposed time complexity is O(N), there N is number of skills.
+    */
+  private def findJob(skills: Seq[String], queues: Map[String, mutable.Queue[(Job, Int)]]): Option[Job] = {
+    val queueWithTheMostRelevantHeadElement: Option[mutable.Queue[(Job, Int)]] = skills
+      // lets get a queue for each skill
+      .map(skill => queues.get(skill))
+      .filter(maybeQueue => maybeQueue.isDefined && maybeQueue.get.nonEmpty)
+      // now get head element's priority (index) from each queue
+      .map(queue => queue.get -> queue.get.head._2)
+      // take the one with the smallest index (the highest priority)
+      .minByOption { case (queue, priority) => priority }
+      .map { case (queue, priority) => queue}
+
+    queueWithTheMostRelevantHeadElement.map(q => q.dequeue()._1)
   }
+
+  /**
+    * Retrieving of the most relevant job for all combinations of skills and job priorities.
+    */
+  def dequeue(agent: Agent): Option[Job] =
+    findJob(agent.primarySkills, highPriority)
+      .orElse(findJob(agent.primarySkills, normalPriority))
+      .orElse(findJob(agent.secondarySkills, highPriority))
+      .orElse(findJob(agent.secondarySkills, normalPriority))
 
 }
--- src/main/scala/bojack/Processor.scala
+++ src/main/scala/bojack/Processor.scala
@@ -4,23 +4,20 @@ import bojack.model.{Agent, Job, Request, Response}
 
 object Processor {
 
-
   def process(agents: Seq[Agent], jobs: Seq[Job], requests: Seq[Request]): Seq[Response] = {
 
-    val initialQueue = new JobQueue(jobs)
+    val jobQueue = new JobQueue(jobs)
 
     val requestedAgents: Seq[Agent] = requests
       .map(r => agents.find(_.id == r.agentId))
       .filter(_.isDefined)
       .map(a => a.get)
 
-    val (_, responseSeq) = requestedAgents.foldLeft((initialQueue, Seq.empty[Response])) { case ((q, seq), a) =>
-      val (maybeJob, iterQueue) = q.dequeue(a)
-      val iterSeq = maybeJob.map(j => Response(a.id, j.id) +: seq).getOrElse(seq)
-      (iterQueue, iterSeq)
-    }
-
-    responseSeq
+    requestedAgents
+      .map(agent => jobQueue.dequeue(agent)
+        .map(job => Response(agent.id, job.id))
+        .getOrElse(Response(agent.id, "Job-not-found"))
+      )
   }
 
 }
